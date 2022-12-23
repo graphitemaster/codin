3097c053df7ef3c2a625c834112a936831754c1e
Dale Weiler <weilercdale@gmail.com>
2022-12-23 09:35:16 -0500

    update unity build script

--- unity.c
+++ unity.c
@@ -1,7 +1,8 @@
 // Unity build script. Just compile this with cl.exe on Windows
 #include "src/array.c"
-#include "src/dump.c"
 #include "src/lexer.c"
 #include "src/parser.c"
 #include "src/report.c"
 #include "src/support.c"
+#include "src/tree.c"
+#include "src/main.c"
