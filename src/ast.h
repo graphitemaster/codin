91bbb573a7b5df7adf455ba31d3180e593c1a215
Dale Weiler <weilercdale@gmail.com>
2023-07-26 19:55:27 -0400

    remove this file

--- src/ast.h
+++
@@ -1,8 +0,0 @@
-#ifndef CODIN_AST_H
-#define CODIN_AST_H
-
-typedef struct Statement Statement;
-typedef struct EmptyStatement EmptyStatement;
-typedef struct ImportStatement ImportStatement;
-
-#endif // CODIN_AST_H
