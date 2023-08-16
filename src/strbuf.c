4eb5302eafe521fd855764b35d5c5762e24a137c
Dale Weiler <weilercdale@gmail.com>
2023-08-15 22:10:55 -0400

    simpler handling of oom

--- src/strbuf.c
+++ src/strbuf.c
@@ -17,40 +17,37 @@ void strbuf_clear(StrBuf *strbuf) {
 	array_clear(strbuf->contents);
 }
 
-Bool strbuf_put_byte(StrBuf *strbuf, Uint8 byte) {
-	return array_push(strbuf->contents, byte);
+void strbuf_put_byte(StrBuf *strbuf, Uint8 byte) {
+	array_push(strbuf->contents, byte);
 }
 
-Bool strbuf_put_rune(StrBuf *strbuf, Rune ch) {
+void strbuf_put_rune(StrBuf *strbuf, Rune ch) {
 	// Decode Rune into individual UTF-8 bytes.
 	if (ch <= 0x7f) {
-		return strbuf_put_byte(strbuf, ch);
+		strbuf_put_byte(strbuf, ch);
 	} else if (ch <= 0x7ff) {
-		return strbuf_put_byte(strbuf, (ch >> 6) & 0x1f)
-		    && strbuf_put_byte(strbuf, ch & 0x3f);
+		strbuf_put_byte(strbuf, (ch >> 6) & 0x1f);
+		strbuf_put_byte(strbuf, ch & 0x3f);
 	} else if (ch <= 0xffff) {
-		return strbuf_put_byte(strbuf, ((ch >> 12) & 0x0f))
-		    && strbuf_put_byte(strbuf, ((ch >> 6) & 0x3f))
-		    && strbuf_put_byte(strbuf, ch & 0x3f);
+		strbuf_put_byte(strbuf, ((ch >> 12) & 0x0f));
+		strbuf_put_byte(strbuf, ((ch >> 6) & 0x3f));
+		strbuf_put_byte(strbuf, ch & 0x3f);
 	} else {
-		return strbuf_put_byte(strbuf, ((ch >> 18) & 0x07))
-		    && strbuf_put_byte(strbuf, ((ch >> 12) & 0x3f))
-		    && strbuf_put_byte(strbuf, ((ch >> 6) & 0x3f))
-		    && strbuf_put_byte(strbuf, ch & 0x3f);
+		strbuf_put_byte(strbuf, ((ch >> 18) & 0x07));
+		strbuf_put_byte(strbuf, ((ch >> 12) & 0x3f));
+		strbuf_put_byte(strbuf, ((ch >> 6) & 0x3f));
+		strbuf_put_byte(strbuf, ch & 0x3f);
 	}
 	UNREACHABLE();
 }
 
-Bool strbuf_put_string(StrBuf *strbuf, String string) {
+void strbuf_put_string(StrBuf *strbuf, String string) {
 	const Size size = array_size(strbuf->contents);
-	if (array_expand(strbuf->contents, string.length)) {
-		memcpy(&strbuf->contents[size], string.contents, string.length);
-		return true;
-	}
-	return false;
+	array_expand(strbuf->contents, string.length);
+	memcpy(&strbuf->contents[size], string.contents, string.length);
 }
 
-Bool strbuf_put_int(StrBuf *strbuf, Sint32 i, Sint32 base) {
+void strbuf_put_int(StrBuf *strbuf, Sint32 i, Sint32 base) {
 	char buffer[sizeof(Sint32) * 8 + 1 + 1];
 	static const char DIGITS[36] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
@@ -72,27 +69,27 @@ Bool strbuf_put_int(StrBuf *strbuf, Sint32 i, Sint32 base) {
 	const Size used = &buffer[sizeof(buffer)] - p;
 	const Size index = array_size(strbuf->contents);
 
-	if (!array_expand(strbuf->contents, used)) {
-		return false;
-	}
+	array_expand(strbuf->contents, used);
 
 	memcpy(&strbuf->contents[index], p, used);
-
-	return true;
 }
 
-void strbuf_put_formatted(StrBuf *strbuf, const char *fmt, ...) {
+void strbuf_put_fmtv(StrBuf *strbuf, const char *fmt, va_list va) {
+	const long bytes = vsnprintf(0, 0, fmt, va) + 1;
+
+	const Size offset = array_size(strbuf->contents);
+	array_expand(strbuf->contents, bytes);
+
+	vsnprintf(RCAST(char *, &strbuf->contents[offset]), bytes, fmt, va);
+
+	array_meta(strbuf->contents)->size--;
+}
+
+void strbuf_put_fmt(StrBuf *strbuf, const char *fmt, ...) {
 	va_list va;
 	va_start(va, fmt);
-	const long bytes = vsnprintf(0, 0, fmt, va);
+	strbuf_put_fmtv(strbuf, fmt, va);
 	va_end(va);
-	const Size size = array_size(strbuf->contents);
-	array_expand(strbuf->contents, bytes + 1);
-	va_list ap;
-	va_start(ap, fmt);
-	vsnprintf(RCAST(char *, &strbuf->contents[size]), bytes + 1, fmt, ap);
-	array_meta(strbuf->contents)->size--; // Remove NUL from vsnprintf.
-	va_end(ap);
 }
 
 String strbuf_result(StrBuf *strbuf) {
--- src/strbuf.h
+++ src/strbuf.h
@@ -1,5 +1,7 @@
 #ifndef CODIN_STRBUF_H
 #define CODIN_STRBUF_H
+#include <stdarg.h>
+
 #include "string.h"
 #include "array.h"
 
@@ -14,10 +16,11 @@ struct StrBuf {
 void strbuf_init(StrBuf *strbuf, Context *context);
 void strbuf_fini(StrBuf *strbuf);
 void strbuf_clear(StrBuf *strbuf);
-Bool strbuf_put_rune(StrBuf *strbuf, Rune ch);
-Bool strbuf_put_string(StrBuf *strbuf, String string);
-Bool strbuf_put_int(StrBuf *strbuf, Sint32 i, Sint32 base);
+void strbuf_put_rune(StrBuf *strbuf, Rune ch);
+void strbuf_put_string(StrBuf *strbuf, String string);
+void strbuf_put_int(StrBuf *strbuf, Sint32 i, Sint32 base);
+void strbuf_put_fmt(StrBuf *strbuf, const char *fmt, ...);
+void strbuf_put_fmtv(StrBuf *strbuf, const char *fmt, va_list va);
 String strbuf_result(StrBuf *strbuf);
-void strbuf_put_formatted(StrBuf *strbuf, const char *fmt, ...);
 
 #endif // CODIN_STRBUF
